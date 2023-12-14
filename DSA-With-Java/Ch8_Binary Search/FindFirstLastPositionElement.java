4a4c6213c3082efc3af9b83d2f3907528aa5d740
Susmita-Dey <susmitadey475@gmail.com>
2023-12-14 12:02:50 +0530

    update FindFirstLastPositionElement solution

--- DSA-With-Java/Ch8_Binary Search/FindFirstLastPositionElement.java
+++ DSA-With-Java/Ch8_Binary Search/FindFirstLastPositionElement.java
@@ -8,8 +8,45 @@ public class FindFirstLastPositionElement {
 
     public static int[] searchRange(int[] nums, int target) {
         int[] ans = { -1, -1 };
-        ans[0] = findFirstOccurence(nums, target);
-        ans[1] = findLastOccurence(nums, target);
+
+        // ans[0] = findFirstOccurence(nums, target);
+        // ans[1] = findLastOccurence(nums, target);
+
+        int startValue = search(nums, target, true);
+        int endValue = search(nums, target, false);
+
+        ans[0] = startValue;
+        ans[1] = endValue;
+
+        return ans;
+    }
+
+    // binarySearch method
+    public static int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2; // here's a problem as it might be possible that
+            // (start + end) exceeds the range of int in Java
+
+            int mid = start + (end - start) / 2; // better way to find mid
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
         return ans;
     }
 
